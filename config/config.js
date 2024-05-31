a8efd1a5497593e8cf3c309e8cbd1e61df3c434a
Mattai16 <111460191+Mattai16@users.noreply.github.com>
2024-05-31 14:18:59 -0600

    :construction: Api node

---
+++ config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    development: {
+      username: "netflix_user",
+      password: "netfli",
+      database: "netflixnode",
+      host: "localhost",
+      port: 3306,
+      dialect: 'mysql'
+    },
+    test: {
+      username: "netflix_user",
+      password: "netfli",
+      database: "netflixnode",
+      host: "localhost",
+      port: 3306,
+      dialect: 'mysql'
+    },
+    production: {
+      username: "netflix_user",
+      password: "netfli",
+      database: "netflixnode",
+      host: "localhost",
+      port: 3306,
+      dialect: 'mysql'
+    },
+  }
